88348236c5e4d206913f0ed1e400b9ac392c20c7
TarikJs <tarikzukic5@gmail.com>
2025-05-06 09:56:51 -0700

    Initial Commit

---
+++ Digit/DigitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitApp.swift
+//  Digit
+//
+//  Created by Tarik Zukic on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DigitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
